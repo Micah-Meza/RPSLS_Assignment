db53207c77a98febfe507b0b79bd72784f3f3639
Micah <airrenno@yahoo.com>
2022-11-20 00:03:30 -0600

    Created display methods for the title and rules. Also created the init player method.

---
+++ game.py
@@ -0,0 +1,52 @@
+import time
+
+class Game:
+
+    def __init__(self) -> None:
+        pass
+
+
+
+    def display_title(self):
+        print()
+        print()
+        print("Welcome to Rock, Paper, Scissors, Lizard, Spock.")
+        time.sleep(1.0)
+        print("Only one can Win!")
+        time.sleep(1.0)
+        print()
+
+    def display_rules(self):
+        print()
+        print("How to play... ")
+        print()
+        time.sleep(1.0)
+        print("Scissors cuts paper.")
+        time.sleep(1.0)
+        print("Paper cover Rock.")
+        time.sleep(1.0)
+        print("Rock crushes Lizard.")
+        time.sleep(1.0)
+        print("Lizard poisons Spock.")
+        time.sleep(1.0)
+        print("Spock smashes Scissors.")
+        time.sleep(1.0)
+        print("Scissors decapitates Lizard.")
+        time.sleep(1.0)
+        print("Lizard eats paper.")
+        time.sleep(1.0)
+        print("Paper disproves Spock.")
+        time.sleep(1.0)
+        print("Spock vaporizes Rock.")
+        time.sleep(1.0)
+        print("Rock crushes Scissors.")
+
+        print()
+        print()
+
+
+
+
+
+
+
